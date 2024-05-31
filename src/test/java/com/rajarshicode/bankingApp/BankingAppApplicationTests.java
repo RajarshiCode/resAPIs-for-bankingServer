65f165713cab9183866e408ef436e37cdd0db2c1
RajarshiCode <rajarshidas47@gmail.com>
2024-05-31 11:04:12 +0530

    started with restAPIs

---
+++ src/test/java/com/rajarshicode/bankingApp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajarshicode.bankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
